a6c20837fa8ce3872479f1be9944348a7d9ec0ff
Uriel Abraham <abraham2160@hotmail.com>
2025-01-31 09:07:43 -0700

    Arreglos

--- POO_IN/src/main/java/com/mycompany/poo_in/POO_IN.java
+++ POO_IN/src/main/java/com/mycompany/poo_in/POO_IN.java
@@ -1,5 +1,6 @@
 package com.mycompany.poo_in;
 
+
 public class POO_IN {
 
     public static void main(String[] args) {
@@ -10,7 +11,7 @@ public class POO_IN {
         String texto = "";
         */
         int arreglo[] = {5, 1, 8, 3, 2, 20, 15};
-        int arreglo2[] = new int[7];
+        int arreglo2[] = new int[arreglo.length];
         //arreglo[5] = 1;
         //System.out.println(arreglo[5]);
         int mayor = 0, pos = 0;
@@ -26,5 +27,14 @@ public class POO_IN {
             mayor = 0;
             System.out.println(arreglo2[i]);
         }
+        System.out.println("-------------------------------------------");
+        int con = 0;
+        for (int i = arreglo.length -1; i >= 0; i--) {
+            arreglo[con] = arreglo2[i];
+            System.out.println(arreglo[con]);
+            con++;
+        }
+        
+        int arreglo3[][] = new int [3][2];
     }
 }
