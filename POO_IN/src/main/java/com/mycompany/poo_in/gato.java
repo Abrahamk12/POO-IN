58bcec26a86a29f54b3172f2d326b360dc709a80
Uriel Abraham <abraham2160@hotmail.com>
2025-03-07 10:18:20 -0700

    JOptopnPane

--- POO_IN/src/main/java/com/mycompany/poo_in/gato.java
+++ POO_IN/src/main/java/com/mycompany/poo_in/gato.java
@@ -1,9 +1,22 @@
 package com.mycompany.poo_in;
-public class gato implements avion{
-    public gato(){}
+import javax.swing.JOptionPane;
 
+public class gato implements avion{
+    private String nombre, raza, color;
+    private int edad, tamaño, peso;
+    
+    public gato(){}
+    
+    public void setNombre(){
+        this.nombre = JOptionPane.showInputDialog(null, "¿Cómo se llamae el gato?");
+    }
+    
+    public void mostrarNombre(){
+        JOptionPane.showMessageDialog(null,"El gato se llama: "+this.nombre);
+    }
+    
     @Override
     public void volar() {
-        
+        System.out.println("El gato volo");
     }
 }
