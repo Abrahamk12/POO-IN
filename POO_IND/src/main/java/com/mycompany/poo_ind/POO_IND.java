ff8e84b415667deba60911a894b9740fb1bcf520
Uriel Abraham <abraham2160@hotmail.com>
2025-03-14 09:41:22 -0700

    .

--- POO_IND/src/main/java/com/mycompany/poo_ind/POO_IND.java
+++ POO_IND/src/main/java/com/mycompany/poo_ind/POO_IND.java
@@ -1,6 +1,5 @@
 package com.mycompany.poo_ind;
 import java.util.ArrayList;
-
 public class POO_IND {
     public static void main(String[] args) {
         ArrayList <String> lista = new ArrayList <String>();
@@ -10,7 +9,7 @@ public class POO_IND {
         System.out.println("Los datos de las lista son: " + lista);
         String d_Lista = lista.get(0);
         System.out.println("El primer dato de la lista es: " + d_Lista +
-                "El tamano de la lista es: " + lista.size());
+                "\tEl tamano de la lista es: " + lista.size());
         lista.remove("Tomate");
         System.out.println("La nueva lista tiene: " + lista+"\n");
         for(String elemento : lista){
