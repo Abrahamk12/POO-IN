4ad6833a0683d878b09a000e5fe9b47ceadfc949
Uriel Abraham <abraham2160@hotmail.com>
2025-03-21 09:35:41 -0700

    JOptionPane
    Integración de cuadros de dialogo

--- POO_IND/src/main/java/com/mycompany/poo_ind/POO_IND.java
+++ POO_IND/src/main/java/com/mycompany/poo_ind/POO_IND.java
@@ -1,9 +1,13 @@
 package com.mycompany.poo_ind;
 import java.util.HashSet;
 import java.util.LinkedList;
+
+import javax.swing.JOptionPane;
+
 public class POO_IND {
     public static void metodoLinkedList(){
         LinkedList<String> lista = new LinkedList<>();
+        String datos = "";
         lista.add("Hola");
         lista.add("Mundo");
 
@@ -15,17 +19,23 @@ public class POO_IND {
         System.out.println("Lista: " + lista);
         for(String elemento : lista){
             System.out.println("Elemento: " + elemento);
+            datos += ", " + elemento;
         }
+        JOptionPane.showMessageDialog(null, datos);
+
     }
     public static void metodoHashSet(){
         HashSet<String> lista = new HashSet<>();
+        String datos = "";
         lista.add("Elemento hashset 1");
         lista.add("Patata");
 
         System.out.println("Lista: " + lista);
         for(String elemento : lista){
             System.out.println("Elemento: " + elemento);
+            datos += ", " + elemento;
         }
+        JOptionPane.showMessageDialog(null, datos);
     }
     public static void main(String[] args) {
         System.out.println("LinkedList");
