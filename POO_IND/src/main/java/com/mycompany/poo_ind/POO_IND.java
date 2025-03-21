ab2b302489273c469eb9bb6069821b714c3ccb9e
Uriel Abraham <abraham2160@hotmail.com>
2025-03-21 09:28:37 -0700

    LinkedList y HashSet

--- POO_IND/src/main/java/com/mycompany/poo_ind/POO_IND.java
+++ POO_IND/src/main/java/com/mycompany/poo_ind/POO_IND.java
@@ -1,19 +1,37 @@
 package com.mycompany.poo_ind;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 public class POO_IND {
-    public static void main(String[] args) {
-        ArrayList <String> lista = new ArrayList <String>();
-        lista.add("Manzana");
-        lista.add("Tomate");
-        lista.add("Jamon");
-        System.out.println("Los datos de las lista son: " + lista);
-        String d_Lista = lista.get(0);
-        System.out.println("El primer dato de la lista es: " + d_Lista +
-                "\tEl tamano de la lista es: " + lista.size());
-        lista.remove("Tomate");
-        System.out.println("La nueva lista tiene: " + lista+"\n");
+    public static void metodoLinkedList(){
+        LinkedList<String> lista = new LinkedList<>();
+        lista.add("Hola");
+        lista.add("Mundo");
+
+        //Añadir cosas al incio de la lista
+        lista.addFirst("Inicio");
+        //Añadir cosas al final de la lista
+        lista.addLast("Fin");
+
+        System.out.println("Lista: " + lista);
         for(String elemento : lista){
-            System.out.println(elemento);
+            System.out.println("Elemento: " + elemento);
         }
+    }
+    public static void metodoHashSet(){
+        HashSet<String> lista = new HashSet<>();
+        lista.add("Elemento hashset 1");
+        lista.add("Patata");
+
+        System.out.println("Lista: " + lista);
+        for(String elemento : lista){
+            System.out.println("Elemento: " + elemento);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("LinkedList");
+        metodoLinkedList();
+        System.out.println("------------------------------------");
+        System.out.println("HashSet");
+        metodoHashSet();
     }
 }
